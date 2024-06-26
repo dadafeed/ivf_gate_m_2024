3eb0cedee166e0c293374c13e1a2da028ff80c45
hjy <jennylady@naver.com>
2024-06-26 22:01:44 +0900

    Upload files

---
+++ asset/js/main.js
@@ -0,0 +1,22 @@
+
+// function layerPopupOpen(e){
+//     $('body').css({"height":"100%","overflow":"hidden"});
+
+//     var pop = $(e);
+//     pop.css("display","block");
+//     pop.stop().clearQueue().animate({'opacity': '1'}, 400, 'easeInOutCubic');
+// }
+
+// function layerPopupClose(e){
+//     $('body').removeAttr("style");
+
+//     var pop = $(e);
+//     pop.stop().clearQueue().animate({'opacity': '0'}, 400, 'easeInOutCubic', function(){
+//         pop.removeAttr("style");
+//     });
+// }
+
+
+// $(document).ready(function(){
+
+// });
